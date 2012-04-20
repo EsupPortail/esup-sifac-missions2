3d507f6d0f8cf69642d943cf0daa8ece2d1b33c2
Florent Cailhol <fcailhol@anyware-services.com>
2012-04-20 16:34:33 +0000

    Mise à jour de la JavaDoc
    git-svn-id: https://subversion.renater.fr/esup-sifacmiss2/trunk@148 1ea0db98-6847-4a72-bad2-ce9059f6f1a9

--- esup-sifacmiss2-web-jsf-mixed/src/main/java/org/esupportail/sifacmissions/web/utils/StringUtils.java
+++ esup-sifacmiss2-web-jsf-mixed/src/main/java/org/esupportail/sifacmissions/web/utils/StringUtils.java
@@ -2,16 +2,21 @@ package org.esupportail.sifacmissions.web.utils;
 
 import java.text.Normalizer;
 
+/**
+ * Cette classe permet de faciliter la manipulation des chaînes de caractères.
+ * 
+ * @author Florent Cailhol (Anyware Services)
+ */
 public abstract class StringUtils {
 
 	/**
-	 * Transform the accentuated letters to their non-accentuated form.
+	 * Transforme les lettres accentuées en leur équivalent sans-accent.
 	 * 
-     *  @param str accentuated string
-     *  @return non-accentuated string
-     */
-	public static String removeAccent(String str) {  
+	 * @param str Chaîne de caractères accentuée
+	 * @return Chaîne de caractères sans accent
+	 */
+	public static String removeAccent(String str) {
 		return Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
 	}
-	
+
 }
