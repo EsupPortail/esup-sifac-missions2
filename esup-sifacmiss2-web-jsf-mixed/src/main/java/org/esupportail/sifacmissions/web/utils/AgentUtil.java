55183d31803bdd0adf7869076a24442213411722
Florent Cailhol <fcailhol@anyware-services.com>
2012-04-24 08:48:02 +0000

    Minor improvements
    git-svn-id: https://subversion.renater.fr/esup-sifacmiss2/trunk@157 1ea0db98-6847-4a72-bad2-ce9059f6f1a9

--- esup-sifacmiss2-web-jsf-mixed/src/main/java/org/esupportail/sifacmissions/web/utils/AgentUtil.java
+++ esup-sifacmiss2-web-jsf-mixed/src/main/java/org/esupportail/sifacmissions/web/utils/AgentUtil.java
@@ -3,7 +3,6 @@
  */
 package org.esupportail.sifacmissions.web.utils;
 
-import java.io.Serializable;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -16,46 +15,25 @@ import org.springframework.beans.factory.InitializingBean;
 
 /**
  * @author Yves Deschamps (Universite de Lille 1) - 2010
- * 
  */
-public class AgentUtil implements Serializable, InitializingBean {
+public class AgentUtil implements InitializingBean {
 
-	/**
-	 * For Serialize.
-	 */
-	private static final long serialVersionUID = -1259495940256943174L;
-
-	/**
-	 * For logging.
-	 */
 	private final Logger logger = new LoggerImpl(getClass());
 
-	/**
-	 * The phone family.
-	 */
 	private String phoneFamily;
-
-	/**
-	 * The skins.
-	 */
 	private Map<String, String> skins;
-
-	/**
-	 * True if mobile.
-	 */
 	private boolean mobile;
-
+	
 	/**
-	 * Constructor.
+	 * @param skins the skins to set
 	 */
-	public AgentUtil() {
-		super();
+	public void setSkins(Map<String, String> skins) {
+		this.skins = skins;
 	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		Assert.notNull(skins, "property skins of class "
-				+ this.getClass().getName() + " can not be null");
+		Assert.notNull(skins, "property skins of class " + this.getClass().getName() + " can not be null");
 	}
 
 	/**
@@ -65,50 +43,33 @@ public class AgentUtil implements Serializable, InitializingBean {
 		if (phoneFamily == null) {
 			String agent = null;
 			FacesContext fc = FacesContext.getCurrentInstance();
-			agent = fc.getExternalContext().getRequestHeaderMap()
-					.get("User-Agent");
-			logger.info("User-Agent: " + agent);
+			agent = fc.getExternalContext().getRequestHeaderMap().get("User-Agent");
+			
+			if (logger.isDebugEnabled()) {
+				logger.debug("User-Agent: " + agent);
+			}
+			
 			for (Iterator<String> i = skins.keySet().iterator(); i.hasNext();) {
 				String key = i.next();
 				if (agent != null && agent.indexOf(key) > -1) {
 					phoneFamily = skins.get(key);
 					mobile = true;
+					
 					return phoneFamily;
 				}
 			}
+			
 			phoneFamily = "minimalFamily";
 		}
+		
 		return phoneFamily;
 	}
 
 	/**
-	 * @return the skins
-	 */
-	public Map<String, String> getSkins() {
-		return skins;
-	}
-
-	/**
-	 * @param skins
-	 *            the skins to set
-	 */
-	public void setSkins(Map<String, String> skins) {
-		this.skins = skins;
-	}
-
-	/**
-	 * @return the mobile
+	 * @return true if mobile
 	 */
 	public boolean isMobile() {
 		return mobile;
-	}
-
-	/**
-	 * @param mobile
-	 *            the mobile to set
-	 */
-	public void setMobile(boolean mobile) {
-		this.mobile = mobile;
 	}
 
 }
