9d0ed81aefd2f945fa88bf4570cecdef67c42ddf
Florent Cailhol <fcailhol@anyware-services.com>
2012-04-24 08:58:39 +0000

    git-svn-id: https://subversion.renater.fr/esup-sifacmiss2/trunk@159 1ea0db98-6847-4a72-bad2-ce9059f6f1a9

--- esup-sifacmiss2-web-jsf-mixed/src/main/java/org/esupportail/sifacmissions/web/controllers/MobileMissionController.java
+++ esup-sifacmiss2-web-jsf-mixed/src/main/java/org/esupportail/sifacmissions/web/controllers/MobileMissionController.java
@@ -6,20 +6,24 @@ import org.esupportail.sifacmissions.domain.beans.Mission;
 public class MobileMissionController extends MissionController {
 
 	private Mission currentMission;
-	
+
 	/**
 	 * @return the current mission
 	 */
 	public Mission getCurrentMission() {
 		return currentMission;
 	}
-	
+
 	/**
 	 * @param mission the current mission
 	 */
 	public void setCurrentMission(Mission mission) {
+		// Reset the mission table
+		setMissionsTable(null);
+
+		// Get mission details and update the current one
 		requestMissionDetails(mission);
-		this.currentMission = mission;
+		currentMission = mission;
 	}
 
 }
