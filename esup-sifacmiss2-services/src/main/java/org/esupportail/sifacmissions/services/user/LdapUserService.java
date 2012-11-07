b375e69728a454835ccc01a1d57383d0e83aab10
Florent Cailhol <fcailhol@anyware-services.com>
2012-11-07 15:41:32 +0000

    Improve LdapUserService
    git-svn-id: https://subversion.renater.fr/esup-sifacmiss2/trunk@283 1ea0db98-6847-4a72-bad2-ce9059f6f1a9

--- esup-sifacmiss2-services/src/main/java/org/esupportail/sifacmissions/services/user/LdapUserService.java
+++ esup-sifacmiss2-services/src/main/java/org/esupportail/sifacmissions/services/user/LdapUserService.java
@@ -2,14 +2,12 @@ package org.esupportail.sifacmissions.services.user;
 
 import java.util.List;
 
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-import javax.naming.directory.Attribute;
-import javax.naming.directory.Attributes;
 import javax.naming.directory.SearchControls;
 
 import org.esupportail.sifacmissions.models.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.DistinguishedName;
@@ -20,10 +18,19 @@ import org.springframework.util.Assert;
 
 public class LdapUserService implements UserService, InitializingBean {
 
+    private static final String LDAP_GIVEN_NAME = "givenName";
+    private static final String LDAP_SN = "sn";
+    private static final String LDAP_UID = "uid";
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private LdapTemplate ldapTemplate;
+    private AttributesMapper attributesMapper;
     private String dnSubPath;
     private String objectClass;
-    private String uidAttribute;
+    private String uidAttribute = LDAP_UID;
+    private String firstnameAttribute = LDAP_GIVEN_NAME;
+    private String lastnameAttribute = LDAP_SN;
 
     public void setLdapTemplate(LdapTemplate ldapTemplate) {
         this.ldapTemplate = ldapTemplate;
@@ -41,12 +48,24 @@ public class LdapUserService implements UserService, InitializingBean {
         this.uidAttribute = uidAttribute;
     }
 
+    public void setFirstnameAttribute(String firstnameAttribute) {
+        this.firstnameAttribute = firstnameAttribute;
+    }
+
+    public void setLastnameAttribute(String lastnameAttribute) {
+        this.lastnameAttribute = lastnameAttribute;
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
-        Assert.notNull(ldapTemplate, "property ldapTemplate can not be null");
-        Assert.notNull(dnSubPath, "property dnSubPath can not be null");
-        Assert.notNull(objectClass, "property objectClass can not be null");
-        Assert.notNull(uidAttribute, "property uidAttribute can not be null");
+        Assert.notNull(ldapTemplate, "ldapTemplate is required");
+        Assert.notNull(dnSubPath, "dnSubPath is required");
+        Assert.notNull(objectClass, "objectClass is required");
+        Assert.hasText(uidAttribute, "uidAttribute is required");
+        Assert.hasText(firstnameAttribute, "firstnameAttribute is required");
+        Assert.hasText(lastnameAttribute, "lastnameAttribute is required");
+
+        attributesMapper = new LdapUserAttributesMapper(uidAttribute, firstnameAttribute, lastnameAttribute);
     }
 
     @SuppressWarnings("unchecked")
@@ -58,48 +77,21 @@ public class LdapUserService implements UserService, InitializingBean {
         filter.and(new EqualsFilter("objectClass", objectClass));
         filter.and(new EqualsFilter(uidAttribute, uid));
 
-        List<User> users = ldapTemplate.search(dn, filter.encode(), SearchControls.SUBTREE_SCOPE, new UserAttributesMapper());
+        List<User> users = ldapTemplate.search(dn, filter.encode(), SearchControls.SUBTREE_SCOPE, attributesMapper);
 
         if (users.size() != 1) {
+            if (logger.isWarnEnabled()) {
+                logger.warn("Unable to find user with uid {}", uid);
+            }
+
             return null;
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Found user with uid {}", uid);
         }
 
         return users.get(0);
     }
 
-    private class UserAttributesMapper implements AttributesMapper {
-
-        @Override
-        public Object mapFromAttributes(Attributes attributes) throws NamingException {
-            User user = new User();
-
-            user.setLogin(getAttribute(attributes, uidAttribute));
-            user.setNom(getAttribute(attributes, "sn"));
-            user.setPrenom(getAttribute(attributes, "givenName"));
-
-            NamingEnumeration<? extends Attribute> attrs = attributes.getAll();
-            while (attrs.hasMore()) {
-                Attribute attr = attrs.next();
-                Object value = attr.get();
-                if (value != null) {
-                    user.getAttributes().put(attr.getID(), value.toString());
-                }
-            }
-
-            return user;
-        }
-
-        private String getAttribute(Attributes attributes, String attrID) throws NamingException {
-            Attribute attr = attributes.get(attrID);
-            if (attr != null) {
-                Object value = attr.get();
-                if (value != null) {
-                    return value.toString();
-                }
-            }
-
-            return "";
-        }
-
-    }
 }
