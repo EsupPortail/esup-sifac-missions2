2b57d1abbb01fc731a3cb3a7758a8c06ab9f72fc
Florent Cailhol <fcailhol@anyware-services.com>
2012-11-08 10:04:40 +0000

    New LDAP matricule service
    git-svn-id: https://subversion.renater.fr/esup-sifacmiss2/trunk@288 1ea0db98-6847-4a72-bad2-ce9059f6f1a9

--- esup-sifacmiss2-services/src/main/java/org/esupportail/sifacmissions/services/matricule/LdapMatriculeService.java
+++ esup-sifacmiss2-services/src/main/java/org/esupportail/sifacmissions/services/matricule/LdapMatriculeService.java
@@ -1,33 +1,59 @@
 package org.esupportail.sifacmissions.services.matricule;
 
-import org.esupportail.sifacmissions.models.User;
-import org.esupportail.sifacmissions.services.user.UserService;
+import java.util.List;
+
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.SearchControls;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.ldap.core.AttributesMapper;
+import org.springframework.ldap.core.DistinguishedName;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.filter.AndFilter;
+import org.springframework.ldap.filter.EqualsFilter;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 
-/**
- * Service permettant de récupérer le matricule de l'utilisateur depuis un
- * annuaire LDAP.
- *
- * @author Yves Deschamps (Université Lille1 - France)
- * @author Florent Cailhol (Anyware Services)
- */
 public class LdapMatriculeService implements MatriculeService, InitializingBean {
+
+    private static final String CACHE_NAME = LdapMatriculeService.class.getName();
+    private static final String LDAP_OBJECT_CLASS = "Person";
+    private static final String LDAP_UID = "uid";
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private UserService userService;
+    private LdapTemplate ldapTemplate;
+    private String dnSubPath;
+    private String objectClass = LDAP_OBJECT_CLASS;
+    private String uidAttribute = LDAP_UID;
     private String matriculeAttribute;
+    private CacheManager cacheManager;
+    private Cache cache;
 
-    /**
-     * @param userService Service de récupération des utilisateurs
-     */
-    public void setUserService(UserService userService) {
-        this.userService = userService;
+    private AttributesMapper attributesMapper;
+    private DistinguishedName dn;
+
+    public void setLdapTemplate(LdapTemplate ldapTemplate) {
+        this.ldapTemplate = ldapTemplate;
+    }
+
+    public void setDnSubPath(String dnSubPath) {
+        this.dnSubPath = dnSubPath;
+    }
+
+    public void setObjectClass(String objectClass) {
+        this.objectClass = objectClass;
+    }
+
+    public void setUidAttribute(String uidAttribute) {
+        this.uidAttribute = uidAttribute;
     }
 
     /**
@@ -39,28 +65,72 @@ public class LdapMatriculeService implements MatriculeService, InitializingBean 
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        Assert.notNull(userService, "userService is required");
+        Assert.notNull(cacheManager, "cacheManager is required");
+        Assert.notNull(ldapTemplate, "ldapTemplate is required");
+        Assert.hasText(dnSubPath, "dnSubPath is required");
+        Assert.hasText(objectClass, "objectClass is required");
+        Assert.hasText(uidAttribute, "uidAttribute is required");
         Assert.hasText(matriculeAttribute, "matriculeAttribute is required");
+
+        attributesMapper = new LdapMatriculeAttributesMapper();
+        dn = new DistinguishedName(dnSubPath);
+
+        if (!cacheManager.cacheExists(CACHE_NAME)) {
+            cacheManager.addCache(CACHE_NAME);
+        }
+
+        cache = cacheManager.getCache(CACHE_NAME);
     }
 
     @Override
-    public String getMatricule(String id) {
-        User user = userService.getUser(id);
-        String matricule = user.getAttributes().get(matriculeAttribute);
+    public String getMatricule(String uid) {
+        String matricule;
 
-        if (StringUtils.hasText(matricule)) {
-            if (logger.isDebugEnabled()) {
-                logger.debug("Récupération du matricule '{}' pour '{}'", matricule, id);
+        if (cache.get(uid) == null) {
+            AndFilter filter = new AndFilter();
+            filter.and(new EqualsFilter("objectClass", objectClass));
+            filter.and(new EqualsFilter(uidAttribute, uid));
+
+            @SuppressWarnings("unchecked")
+            List<String> matricules = ldapTemplate.search(dn, filter.encode(), SearchControls.SUBTREE_SCOPE, attributesMapper);
+
+            if (matricules.size() != 1) {
+                if (logger.isWarnEnabled()) {
+                    logger.warn("Unable to find matricule with uid {}", uid);
+                }
+
+                return null;
             }
 
-            return matricule;
+            matricule = matricules.get(0);
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Found matricule {} with uid {}", matricule, uid);
+            }
+
+            cache.put(new Element(uid, matricule));
+        } else {
+            matricule = (String) cache.get(uid).getObjectValue();
         }
 
-        if (logger.isWarnEnabled()) {
-            logger.warn("Aucun matricule n'a pu être récupéré pour '{}'", id);
+        return matricule;
+    }
+
+    private class LdapMatriculeAttributesMapper implements AttributesMapper {
+
+        @Override
+        public Object mapFromAttributes(Attributes attributes) throws NamingException {
+            Attribute attr = attributes.get(matriculeAttribute);
+            if (attr != null) {
+                Object value = attr.get();
+                if (value != null) {
+                    return value.toString();
+                }
+            }
+
+            return "";
         }
 
-        return null;
     }
 
 }
